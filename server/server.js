e56d4cf72735d94f053d975389634646c7829401
Ksenia Smith <smithkseniav@gmail.com>
2015-09-23 13:50:53 -0700

    implemented ENV

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var Promise = require('bluebird');
 var request = require('request');
+var ENV = require('../.env');
 
 var app = express();
 var API_BASE = 'http://services.wine.com/api/beta2/service.svc/JSON/catalog?search=';
-var API_KEY = '';
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -15,7 +15,7 @@ app.use(function(req, res, next) {
 app.get('/api/*', function(req, res){
   var query = req.url.replace('/api/', '');
   console.log("query: ", query);
-  request(API_BASE+query+API_KEY, function(err, response, body){
+  request(API_BASE+query+'&apikey='+ENV(), function(err, response, body){
     console.log("body that came back: ", body);
     res.send(body);
   });
