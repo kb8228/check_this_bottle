35f21b6c97d2ed86178507ca8b38c42db1311f22
Ksenia Smith <smithkseniav@gmail.com>
2015-09-23 17:09:54 -0700

    heroku ENV

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ var http = require('http');
 var express = require('express');
 var Promise = require('bluebird');
 var request = require('request');
-var ENV = require('../.env');
 
 var app = express();
 var API_BASE = 'http://services.wine.com/api/beta2/service.svc/JSON/catalog?search=';
@@ -22,7 +21,7 @@ app.get('/', function(req, res){
 app.get('/api/*', function(req, res){
   var query = req.url.replace('/api/', '');
   // console.log("query: ", query);
-  request(API_BASE+query+'&apikey='+ENV(), function(err, response, body){
+  request(API_BASE+query+'&apikey='+ENV[KEY], function(err, response, body){
     // console.log("body that came back: ", body);
     res.send(body);
   });
